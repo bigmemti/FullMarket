419d5386903069ade1c29a7d07b3cb433c043b3b
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2025-04-21 18:31:23 +0330

    refactor show brand

--- resources/js/pages/panel/brand/show.tsx
+++ resources/js/pages/panel/brand/show.tsx
@@ -1,12 +1,11 @@
-import { DialogClose, DialogHeader, DialogTitle, DialogTrigger, Dialog, DialogContent, DialogFooter, DialogDescription } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import AppLayout from "@/layouts/app-layout";
 import { Brand, BreadcrumbItem } from "@/types";
-import { Head, Link } from "@inertiajs/react";
+import { Head, router } from "@inertiajs/react";
 import dayjs from "dayjs";
-import { Pencil, Trash } from "lucide-react";
-
-
+import { EditButton, DeleteButton } from "@/components/dashboard/responsive-list";
+import { useState } from "react";
+import { ConfirmDialog } from "@/components/dashboard/dialog";
+import DashboardHeader from "@/components/dashboard/header";
 export default function Show({ brand }: { brand: Brand }) {
     const breadcrumb: BreadcrumbItem[] =[
         {
@@ -23,14 +22,34 @@ export default function Show({ brand }: { brand: Brand }) {
         },
     ];
 
+    const [deleteDialog, setDeleteDialog] = useState<Partial<{
+        open: boolean;
+        processing: boolean;
+    }>>({
+        open: false,
+        processing: false,
+    });
+    
+    const openDeleteDialog = (): void => {
+        setDeleteDialog({ open: true });
+    }
+    
+    const deleteBrand = (): void => {
+        setDeleteDialog({ processing: true });
+        router.delete(route('panel.brand.destroy', { brand: brand.id }), {
+            preserveScroll: true,
+            onSuccess: () => {
+                setDeleteDialog({ processing: false, open: false });
+            },
+        });
+    }
+
     return (
         <AppLayout breadcrumbs={breadcrumb}>
             <Head title="Brand Show" />
-            <div>
-                <div className="p-4 flex justify-between">
-                    <div className="text-2xl font-bold">Brand Show</div>
-                </div>
-            </div>
+            
+            <DashboardHeader title={`Brand ${brand.name}`} />
+
             <div className="p-4">
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col gap-2">
@@ -50,37 +69,18 @@ export default function Show({ brand }: { brand: Brand }) {
                         <div className="text-sm">{dayjs(brand.updated_at).format('YYYY/MM/DD H:m')}</div>
                     </div>
                     <div className="flex gap-2">
-                        <Link href={route('panel.brand.edit', {brand})} className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground p-3">
-                            <Pencil className="w-4 h-4" />
-                        </Link>
-                        <Dialog>
-                            <DialogTrigger asChild>
-                                <Button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input dark:border-none dark:bg-destructive dark:text-destructive-foreground  dark:hover:bg-destructive/85 p-3 cursor-pointer bg-red-500 hover:bg-red-600 text-black">
-                                    <Trash className="w-4 h-4" />
-                                </Button>
-                            </DialogTrigger>
-                            <DialogContent>
-                                <DialogHeader>
-                                    <DialogTitle>Confirm Deletion</DialogTitle>
-                                    <DialogDescription>
-                                        Are you sure you want to delete this brand? This action cannot be undone.
-                                    </DialogDescription>
-                                </DialogHeader>
-                                <DialogFooter>
-                                    <DialogClose asChild>
-                                        <Button variant="outline" className="mr-2 cursor-pointer">
-                                            Cancel
-                                        </Button>
-                                    </DialogClose>
-                                    <Button className="cursor-pointer" variant="destructive">
-                                        Delete
-                                    </Button>
-                                </DialogFooter>
-                            </DialogContent>
-                        </Dialog>
+                        <EditButton href={route('panel.brand.edit', {brand})} />
+                        <DeleteButton onClick={openDeleteDialog} />
                     </div>
                 </div>
             </div>
+
+            <ConfirmDialog 
+                isOpen={deleteDialog.open || false} 
+                onOpenChange={(open) => setDeleteDialog({ open })} 
+                onDelete={() => deleteBrand()} 
+                processing={deleteDialog.processing || false} 
+            />
         </AppLayout>
     );
 }
